531b6771d6b28b00038dbb7cb0a07135e0bc6233
Hannes Eder <hannes@hanneseder.net>
2020-05-01 16:17:59 +0200

    Factor out almost all code from main and mains

--- trce_src/util.h
+++ trce_src/util.h
@@ -1,29 +1,6 @@
 #ifndef TRCE_SRC_UTIL_H
 #define TRCE_SRC_UTIL_H
 
-#include <stdio.h>
-
-extern FILE *bituit;
-
-extern int lfptr; /* load file pointer */
-
-extern int termbitsize;
-extern char bmbuf[9000];
-
-extern int errflag;
-
-void meldroutine(void);
-void erroutine(void);
-void spare(int t);
-void notim(int t);
-void interrupt(int t);
-void panic(char *s);
-int getint(FILE *f);
-int getsh(FILE *f);
-void bitmapdump(int b, int h, char *buff);
-void bitmapopen(int b, int h, int s);
-void schrijf(void);
-int spiegel(int m, int n);
-void schrijfmap(int b, int h, int s, char *buf, FILE *uitf);
+int util_main(int argc, char **argv);
 
 #endif /* TRCE_SRC_UTIL_H */
