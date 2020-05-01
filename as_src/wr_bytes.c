b45ace8e837ba87789e653acbe6b78440cc14e3d
Hannes Eder <hannes@hanneseder.net>
2020-05-01 22:23:04 +0200

    Fixup wr_bytes.c

--- as_src/wr_bytes.c
+++ as_src/wr_bytes.c
@@ -2,22 +2,19 @@
  * (c) copyright 1987 by the Vrije Universiteit, Amsterdam, The Netherlands.
  * See the copyright notice in the ACK home directory, in the file "Copyright".
  */
+#include <unistd.h>
 
 #include "obj.h"
+#include "comm7.h"
 
 static int maxchunk = 16384;
 
 /*
  * Just write "cnt" bytes to file-descriptor "fd".
  */
-void
-wr_bytes(fd, string, cnt)
-	register char	*string;
-	register long	cnt;
-{
-
+void wr_bytes(int fd, char *string, long cnt) {
 	while (cnt) {
-		register int n = cnt >= maxchunk ? maxchunk : cnt;
+		int n = cnt >= maxchunk ? maxchunk : cnt;
 
 		if (write(fd, string, n) != n)
 			wr_fatal();
