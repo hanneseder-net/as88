0357544921e637a0b60a7fd2323adaea926b7397
Hannes Eder <hannes@hanneseder.net>
2020-05-01 17:24:33 +0200

    proper prototypes for functions in store.c

---
+++ trce_src/store.h
@@ -0,0 +1,11 @@
+#ifndef TRCE_SRC_STORE_H
+#define TRCE_SRC_STORE_H
+
+// TODO(heder): This typedef shouldn't be here.
+typedef short word;
+
+void wstore(word x);
+void xstore(char *x);
+void rapwstore(word w);
+
+#endif /* TRCE_SRC_STORE_H */
