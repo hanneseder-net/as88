c17f4773e489a0996630c77f90a46703483b5a02
Hannes Eder <hannes@hanneseder.net>
2020-05-02 13:23:46 +0200

    Remove a bit of cruft from obj.h

--- as_src/obj.h
+++ as_src/obj.h
@@ -9,16 +9,6 @@
 #include <stdio.h>
 #include "out.h"
 
-#if ! defined(CHAR_UNSIGNED)
-#define CHAR_UNSIGNED 0
-#endif
-
-#if CHAR_UNSIGNED
-#define Xchar(ch)	(ch)
-#else
-#define Xchar(ch)	((ch) & 0377)
-#endif
-
 #if defined(__i386__) || defined(i386)
 #define BYTE_ORDER 0x0123
 #endif
@@ -28,25 +18,19 @@
 #endif
 
 #if (BYTE_ORDER == 0x3210 || BYTE_ORDER == 0x1032)
-#define uget2(c)	(Xchar((c)[0]) | ((unsigned) Xchar((c)[1]) << 8))
 #define Xput2(i, c)	(((c)[0] = (i)), ((c)[1] = (i) >> 8))
 #define put2(i, c)	{ int j = (i); Xput2(j, c); }
 #else
-#define uget2(c)	(* ((unsigned short *) (c)))
 #define Xput2(i, c)	(* ((short *) (c)) = (i))
 #define put2(i, c)	Xput2(i, c)
 #endif
 
-#define get2(c)		((short) uget2(c))
-
 #if BYTE_ORDER != 0x0123
-#define get4(c)		(uget2(c) | ((long) uget2((c)+2) << 16))
 #define put4(l, c)	{ long x=(l); \
 			  Xput2((int)x,c); \
 			  Xput2((int)(x>>16),(c)+2); \
 			}
 #else
-#define get4(c)		(* ((long *) (c)))
 #define put4(l, c)	(* ((long *) (c)) = (l))
 #endif
 
