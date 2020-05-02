5bcae81e767f01f3821d742d3bc06bffb046c835
Hannes Eder <hannes@hanneseder.net>
2020-05-02 11:47:56 +0200

    clang-format and cleanup store.c

--- trce_src/store.c
+++ trce_src/store.c
@@ -1,84 +1,77 @@
 #define EXTERN extern
 #include "88.h"
-#include "macro.h"
 
 void wstore(word x) {
 #ifdef LITTLE_ENDIAN
-    *eapc++ = x;
-    *eapc = x>>8;
+  *eapc++ = x;
+  *eapc = x >> 8;
 #else
-    register reg	t;
+  reg t;
 
-    t.w = x;
+  t.w = x;
 
-    /*
-     * XXX WATCH OUT HERE!!
-     *
-     * This is special depending on the type of machine you have and
-     * whether the registers fall below memory or above memory in the
-     * final link map
-     */
+  /*
+   * XXX WATCH OUT HERE!!
+   *
+   * This is special depending on the type of machine you have and whether the
+   * registers fall below memory or above memory in the final link map
+   */
 
-    if (eapc >= M && eapc < M + MEMBYTES) {
-		/* memory store is different from register */
-	    *eapc++ = t.b.lo;
-	    *eapc = t.b.hi;
-    }
-    else {
-	*eapc++ = t.b.hi;
-	*eapc = t.b.lo;
-    }
+  if (eapc >= M && eapc < M + MEMBYTES) {
+    /* memory store is different from register */
+    *eapc++ = t.b.lo;
+    *eapc = t.b.hi;
+  } else {
+    *eapc++ = t.b.hi;
+    *eapc = t.b.lo;
+  }
 #endif
 }
 
 void xstore(char *x) {
 #ifdef LITTLE_ENDIAN
-    *eapc= *x;
-    *(eapc+1)= *(x+1);
+  *eapc = *x;
+  *(eapc + 1) = *(x + 1);
 #else
-    register reg t;
+  reg t;
 
-    t.w = *(word *)x;
-    /*
-     * XXX WATCH OUT HERE!!
-     *
-     * This is special depending on the type of machine you have and
-     * whether the registers fall below memory or above memory in the
-     * final link map
-     */
-    if (eapc <= M + MEMBYTES) {	/* memory store is different from register */
-	    *eapc = t.b.hi;
-	    *(eapc+1) = t.b.lo;
-    }
-    else {
-	*eapc = t.b.lo;
-	*(eapc+1) = t.b.hi;
-    }
-#endif /*!LITTLE_ENDIAN*/
+  t.w = *(word *)x;
+  /*
+   * XXX WATCH OUT HERE!!
+   *
+   * This is special depending on the type of machine you have and whether the
+   * registers fall below memory or above memory in the final link map
+   */
+  if (eapc <= M + MEMBYTES) { /* memory store is different from register */
+    *eapc = t.b.hi;
+    *(eapc + 1) = t.b.lo;
+  } else {
+    *eapc = t.b.lo;
+    *(eapc + 1) = t.b.hi;
+  }
+#endif
 }
 
 void rapwstore(word w) {
 #ifdef LITTLE_ENDIAN
-    *rapw = w;
+  *rapw = w;
 #else
-    register reg	t;
-    register char	*ptr = (char *)rapw;
+  reg t;
+  char *ptr = (char *)rapw;
 
-    t.w = w;
-    /*
-     * XXX WATCH OUT HERE!!
-     *
-     * This is special depending on the type of machine you have and
-     * whether the registers fall below memory or above memory in the
-     * final link map
-     */
-    if (ptr <= M + MEMBYTES) {	/* memory store is different from register */
-	    *ptr++ = t.b.lo;
-	    *ptr = t.b.hi;
-    }
-    else {
-	*ptr++ = t.b.hi;
-	*ptr = t.b.lo;
-    }
+  t.w = w;
+  /*
+   * XXX WATCH OUT HERE!!
+   *
+   * This is special depending on the type of machine you have and whether the
+   * registers fall below memory or above memory in the final link map
+   */
+  if (ptr <= M + MEMBYTES) { /* memory store is different from register */
+    *ptr++ = t.b.lo;
+    *ptr = t.b.hi;
+  } else {
+    *ptr++ = t.b.hi;
+    *ptr = t.b.lo;
+  }
 #endif
 }
