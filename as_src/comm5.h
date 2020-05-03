2dcde19cc46924ec7f0bbc97df45e4d5b3651108
Hannes Eder <hannes@hanneseder.net>
2020-05-03 20:44:59 +0200

    clang-format

--- as_src/comm5.h
+++ as_src/comm5.h
@@ -3,14 +3,14 @@
 
 #include "comm0.h"
 
-int hash(char *p);
+int hash(char* p);
 int nextchar(void);
 void putval(int c);
 item_t* fb_alloc(int lab);
-item_t * fb_shift(int lab);
+item_t* fb_shift(int lab);
 item_t* item_alloc(int typ);
-void item_insert(item_t *ip, int h);
-item_t* item_search(char *p);
+void item_insert(item_t* ip, int h);
+item_t* item_search(char* p);
 
 int yylex(void);
 
