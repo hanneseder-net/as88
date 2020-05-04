6a803d8753eacb73da6f3d36b381a8b21c17ccae
Hannes Eder <hannes@hanneseder.net>
2020-05-04 23:17:17 +0200

    clang-format

--- as_src/comm7.c
+++ as_src/comm7.c
@@ -17,210 +17,195 @@
 #include "y.tab.h"
 #include "wr.h"
 
-valu_t load(item_t *ip) {
+valu_t load(item_t* ip) {
 #ifdef ASLD
-	int typ;
+  int typ;
 
-	typ = ip->i_type & S_TYP;
-	if ((typ -= S_MIN) < 0)		/* S_UND or S_ABS */
-		return(ip->i_valu);
-	return(ip->i_valu + sect[typ].s_base);
+  typ = ip->i_type & S_TYP;
+  if ((typ -= S_MIN) < 0) /* S_UND or S_ABS */
+    return (ip->i_valu);
+  return (ip->i_valu + sect[typ].s_base);
 #else
-	if ((ip->i_type & S_TYP) == S_UND || (ip->i_type & S_COM)) {
-		if (pass == PASS_3) {
-			if (relonami != 0)
-				serror("relocation error");
-			relonami = ip->i_valu+1;
-		}
-		return(0);
-	}
-	return(ip->i_valu);
+  if ((ip->i_type & S_TYP) == S_UND || (ip->i_type & S_COM)) {
+    if (pass == PASS_3) {
+      if (relonami != 0) serror("relocation error");
+      relonami = ip->i_valu + 1;
+    }
+    return (0);
+  }
+  return (ip->i_valu);
 #endif
 }
 
-int store(item_t *ip, valu_t val) {
+int store(item_t* ip, valu_t val) {
 #ifdef ASLD
-	int typ;
+  int typ;
 
-	typ = ip->i_type & S_TYP;
-	if ((typ -= S_MIN) >= 0)
-		val -= sect[typ].s_base;
+  typ = ip->i_type & S_TYP;
+  if ((typ -= S_MIN) >= 0) val -= sect[typ].s_base;
 #else
-	if ((ip->i_type & S_TYP) == S_UND)
-		return(0);
+  if ((ip->i_type & S_TYP) == S_UND) return (0);
 #endif
-	assert(pass != PASS_3 || (ip->i_type & S_VAR) || ip->i_valu == val);
-	ip->i_valu = val;
-	return(1);
+  assert(pass != PASS_3 || (ip->i_type & S_VAR) || ip->i_valu == val);
+  ip->i_valu = val;
+  return (1);
 }
 
-char* remember(char *s) {
-	char *p;
-	int n;
-	static int nleft = 0;
-	static char *next;
+char* remember(char* s) {
+  char* p;
+  int n;
+  static int nleft = 0;
+  static char* next;
 
-	p = s;
-	n = 0;
-	do
-		n++;
-	while (*p++);
-	if ((nleft -= n) < 0) {
-		next = malloc(MEMINCR);
-		if (next == 0)
-			fatal("out of memory");
-		nleft = (MEMINCR / sizeof(char)) - n;
-		assert(nleft >= 0);
-	}
-	p = next;
-	while ((*p++ = *s++))
-		;
-	s = next;
-	next = p;
-	return(s);
+  p = s;
+  n = 0;
+  do
+    n++;
+  while (*p++);
+  if ((nleft -= n) < 0) {
+    next = malloc(MEMINCR);
+    if (next == 0) fatal("out of memory");
+    nleft = (MEMINCR / sizeof(char)) - n;
+    assert(nleft >= 0);
+  }
+  p = next;
+  while ((*p++ = *s++))
+    ;
+  s = next;
+  next = p;
+  return (s);
 }
 
 int combine(int typ1, int typ2, int op) {
-	switch (op) {
-	case '+':
-		if (typ1 == S_ABS)
-			return(typ2);
-		if (typ2 == S_ABS)
-			return(typ1);
-		break;
-	case '-':
-		if (typ2 == S_ABS)
-			return(typ1);
-		if ((typ1 & ~S_DOT) == (typ2 & ~S_DOT) && typ1 != S_UND)
-			return(S_ABS|S_VAR);
-		break;
-	case '>':
-		if (typ1 == S_ABS && typ2 == S_ABS)
-			return(S_ABS);
-		if (
-		    ((typ1 & ~S_DOT) == (typ2 & ~S_DOT) && typ1 != S_UND)
-		    || (typ1 == S_ABS)
-		    || (typ2 == S_ABS)
-		   )
-			return(S_ABS|S_VAR);
-		break;
-	default:
-		if (typ1 == S_ABS && typ2 == S_ABS)
-			return(S_ABS);
-		break;
-	}
-	if (pass != PASS_1)
-		serror("illegal operator");
-	return(S_UND);
+  switch (op) {
+    case '+':
+      if (typ1 == S_ABS) return (typ2);
+      if (typ2 == S_ABS) return (typ1);
+      break;
+    case '-':
+      if (typ2 == S_ABS) return (typ1);
+      if ((typ1 & ~S_DOT) == (typ2 & ~S_DOT) && typ1 != S_UND)
+        return (S_ABS | S_VAR);
+      break;
+    case '>':
+      if (typ1 == S_ABS && typ2 == S_ABS) return (S_ABS);
+      if (((typ1 & ~S_DOT) == (typ2 & ~S_DOT) && typ1 != S_UND) ||
+          (typ1 == S_ABS) || (typ2 == S_ABS))
+        return (S_ABS | S_VAR);
+      break;
+    default:
+      if (typ1 == S_ABS && typ2 == S_ABS) return (S_ABS);
+      break;
+  }
+  if (pass != PASS_1) serror("illegal operator");
+  return (S_UND);
 }
 
 #ifdef LISTING
 int printx(int ndig, valu_t val) {
-	static char buf[8];
-	char *p;
-	int c, n;
+  static char buf[8];
+  char* p;
+  int c, n;
 
-	p = buf; n = ndig;
-	do {
-		*p++ = (int) val & 017;
-		val >>= 4;
-	} while (--n);
-	do {
-		c = "0123456789ABCDEF"[(int)*--p];
-		putchar(c);
-	} while (p > buf);
-	return(ndig);
+  p = buf;
+  n = ndig;
+  do {
+    *p++ = (int)val & 017;
+    val >>= 4;
+  } while (--n);
+  do {
+    c = "0123456789ABCDEF"[(int)*--p];
+    putchar(c);
+  } while (p > buf);
+  return (ndig);
 }
 #endif
 
 #ifdef LISTING
 void listline(int textline) {
-	int c;
+  int c;
 
-	if ((listflag & 4) && (c = getc(listfile)) != '\n' && textline) {
-		if (listcolm >= 24)
-			printf(" \\\n\t\t\t");
-		else
-			do {
-				putchar('\t');
-				listcolm += 8;
-			} while (listcolm < 24);
-		do {
-			assert(c != EOF);
-			putchar(c);
-		} while ((c = getc(listfile)) != '\n');
-	}
-	if (listflag & 7) {
-		putchar('\n');
-		fflush(stdout);
-	}
-	listeoln = 1;
-	listcolm = 0;
-	listflag = listtemp;
+  if ((listflag & 4) && (c = getc(listfile)) != '\n' && textline) {
+    if (listcolm >= 24)
+      printf(" \\\n\t\t\t");
+    else
+      do {
+        putchar('\t');
+        listcolm += 8;
+      } while (listcolm < 24);
+    do {
+      assert(c != EOF);
+      putchar(c);
+    } while ((c = getc(listfile)) != '\n');
+  }
+  if (listflag & 7) {
+    putchar('\n');
+    fflush(stdout);
+  }
+  listeoln = 1;
+  listcolm = 0;
+  listflag = listtemp;
 }
 #endif /* LISTING */
 
 /* ---------- code optimization ---------- */
 
 #ifdef THREE_PASS
-#define PBITTABSZ	128
-static char *pbittab[PBITTABSZ];
+#define PBITTABSZ 128
+static char* pbittab[PBITTABSZ];
 
 int small(int fitsmall, int gain) {
-	int bit;
-	char *p;
+  int bit;
+  char* p;
 
-	if (DOTSCT == NULL)
-		nosect();
-	if (bflag)
-		return(0);
-	if (nbits == BITCHUNK) {
-		bitindex++;
-		nbits = 0;
-		if (bitindex == PBITTABSZ) {
-			static int w_given;
-			if (pass == PASS_1 && ! w_given) {
-				w_given = 1;
-				warning("bit table overflow");
-			}
-			return(0);
-		}
-		if (pbittab[bitindex] == 0 && pass == PASS_1) {
-			if ((pbittab[bitindex] = calloc(MEMINCR, 1)) == 0) {
-				static int w2_given;
+  if (DOTSCT == NULL) nosect();
+  if (bflag) return (0);
+  if (nbits == BITCHUNK) {
+    bitindex++;
+    nbits = 0;
+    if (bitindex == PBITTABSZ) {
+      static int w_given;
+      if (pass == PASS_1 && !w_given) {
+        w_given = 1;
+        warning("bit table overflow");
+      }
+      return (0);
+    }
+    if (pbittab[bitindex] == 0 && pass == PASS_1) {
+      if ((pbittab[bitindex] = calloc(MEMINCR, 1)) == 0) {
+        static int w2_given;
 
-				if (!w2_given) {
-					w2_given = 1;
-					warning("out of space for bit table");
-				}
-			}
-		}
-		if (pbittab[bitindex] == 0)
-			return (0);
-	}
-	bit = 1 << (nbits&7);
-	p = pbittab[bitindex]+(nbits>>3);
-	nbits++;
-	switch (pass) {
-	case PASS_1:
-		return(0);
-	case PASS_2:
-		if (fitsmall) {
-			DOTGAIN += gain;
-			*p |= bit;
-		}
-		return(fitsmall);
-	case PASS_3:
-		assert(fitsmall || (*p & bit) == 0);
-		return(*p & bit);
-	}
-	/*NOTREACHED*/
-	return 0;
+        if (!w2_given) {
+          w2_given = 1;
+          warning("out of space for bit table");
+        }
+      }
+    }
+    if (pbittab[bitindex] == 0) return (0);
+  }
+  bit = 1 << (nbits & 7);
+  p = pbittab[bitindex] + (nbits >> 3);
+  nbits++;
+  switch (pass) {
+    case PASS_1:
+      return (0);
+    case PASS_2:
+      if (fitsmall) {
+        DOTGAIN += gain;
+        *p |= bit;
+      }
+      return (fitsmall);
+    case PASS_3:
+      assert(fitsmall || (*p & bit) == 0);
+      return (*p & bit);
+  }
+  /*NOTREACHED*/
+  return 0;
 }
-#else 
+#else
 int small(int fitsmall, int gain) {
-	// TODO(heder): Not sure about the return value here.
-	return 0;
+  // TODO(heder): Not sure about the return value here.
+  return 0;
 }
 #endif
 
@@ -229,7 +214,7 @@ int small(int fitsmall, int gain) {
 extern int __sectionnr;
 
 static void wr_putc(int ch) {
-  struct fil *ptr = &__parts[PARTEMIT + getsect(__sectionnr)];
+  struct fil* ptr = &__parts[PARTEMIT + getsect(__sectionnr)];
 
   if (ptr->cnt == 0) __wr_flush(ptr);
   ptr->cnt--;
@@ -280,101 +265,101 @@ void emit1(int arg) {
 
 void emit2(int arg) {
 #ifdef BYTES_REVERSED
-	emit1((arg>>8)); emit1(arg);
+  emit1((arg >> 8));
+  emit1(arg);
 #else
-	emit1(arg); emit1((arg>>8));
+  emit1(arg);
+  emit1((arg >> 8));
 #endif
 }
 
 void emit4(long arg) {
 #ifdef WORDS_REVERSED
-	emit2((int)(arg>>16)); emit2((int)(arg));
+  emit2((int)(arg >> 16));
+  emit2((int)(arg));
 #else
-	emit2((int)(arg)); emit2((int)(arg>>16));
+  emit2((int)(arg));
+  emit2((int)(arg >> 16));
 #endif
 }
 
 void emitx(valu_t val, int n) {
-	switch (n) {
-	case 1:
-		emit1((int)val); break;
-	case 2:
+  switch (n) {
+    case 1:
+      emit1((int)val);
+      break;
+    case 2:
 #ifdef BYTES_REVERSED
-		emit1(((int)val>>8)); emit1((int)val);
+      emit1(((int)val >> 8));
+      emit1((int)val);
 #else
-		emit1((int)val); emit1(((int)val>>8));
+      emit1((int)val);
+      emit1(((int)val >> 8));
 #endif
-		break;
-	case 4:
+      break;
+    case 4:
 #ifdef WORDS_REVERSED
-		emit2((int)(val>>16)); emit2((int)(val));
+      emit2((int)(val >> 16));
+      emit2((int)(val));
 #else
-		emit2((int)(val)); emit2((int)(val>>16));
+      emit2((int)(val));
+      emit2((int)(val >> 16));
 #endif
-		break;
-	default:
-		assert(0);
-	}
+      break;
+    default:
+      assert(0);
+  }
 }
 
 void emitstr(int zero) {
-	int i;
-	char *p;
+  int i;
+  char* p;
 
-	p = stringbuf;
-	i = stringlen;
-	while (--i >= 0)
-		emit1(*p++);
-	if (zero)
-		emit1(0);
+  p = stringbuf;
+  i = stringlen;
+  while (--i >= 0) emit1(*p++);
+  if (zero) emit1(0);
 }
 
 /* ---------- Error checked file I/O  ---------- */
 
-void ffreopen(char *s, FILE *f) {
-	if (freopen(s, "r", f) == NULL)
-		fatal("can't reopen %s", s);
+void ffreopen(char* s, FILE* f) {
+  if (freopen(s, "r", f) == NULL) fatal("can't reopen %s", s);
 }
 
-FILE* ffcreat(char *s) {
-	FILE *f;
+FILE* ffcreat(char* s) {
+  FILE* f;
 
-	if ((f = fopen(s, "w")) == NULL)
-		fatal("can't create %s", s);
-	return(f);
+  if ((f = fopen(s, "w")) == NULL) fatal("can't create %s", s);
+  return (f);
 }
 
 #ifndef TMPDIR
 #define TMPDIR "/tmp"
 #endif
-char *tmp_dir = TMPDIR;
+char* tmp_dir = TMPDIR;
 
-FILE* fftemp(char *path, char *tail) {
-	char *dir;
+FILE* fftemp(char* path, char* tail) {
+  char* dir;
 
-	if ((dir = getenv("TMPDIR")) == NULL)
-		dir = tmp_dir;
-	sprintf(path, "%s/%s", dir, tail);
-	mkstemp(path);
-	return(ffcreat(path));
+  if ((dir = getenv("TMPDIR")) == NULL) dir = tmp_dir;
+  sprintf(path, "%s/%s", dir, tail);
+  mkstemp(path);
+  return (ffcreat(path));
 }
 
 /* ---------- Error handling ---------- */
 
-void yyerror(char const *s) {
+void yyerror(char const* s) {
   UNUSED(s);
   /* we will do our own error printing */
-}		
-
-void nosect(void) {
-	fatal("no sections");
 }
 
-void wr_fatal(void) {
-	fatal("write error");
-}
+void nosect(void) { fatal("no sections"); }
 
-static void diag_va(char *tail, char *s, va_list argp) {
+void wr_fatal(void) { fatal("write error"); }
+
+static void diag_va(char* tail, char* s, va_list argp) {
   fflush(stdout);
   if (modulename)
     fprintf(stderr, "\"%s\", line %ld: ", modulename, lineno);
@@ -384,45 +369,43 @@ static void diag_va(char *tail, char *s, va_list argp) {
   fprintf(stderr, "%s", tail);
 }
 
-void fatal(char *s, ...) {
-	va_list argp;
-	va_start(argp, s);
-	nerrors++;
-	diag_va(" (fatal)\n", s, argp);
-	va_end(argp);
-	stop(0);
+void fatal(char* s, ...) {
+  va_list argp;
+  va_start(argp, s);
+  nerrors++;
+  diag_va(" (fatal)\n", s, argp);
+  va_end(argp);
+  stop(0);
 }
 
 #if DEBUG == 2
-void assert2(char *file, int line) {
-	fatal("assertion failed (%s, %d)", file, line);
+void assert2(char* file, int line) {
+  fatal("assertion failed (%s, %d)", file, line);
 }
 #endif
 
 #if DEBUG == 1
-assert1()
-{
-	diag(" (fatal)\n", "assertion failed");
-	abort();
+assert1() {
+  diag(" (fatal)\n", "assertion failed");
+  abort();
 }
 #endif
 
 void serror(char* s, ...) {
-	va_list argp;
-	va_start(argp, s);
-	nerrors++;
-	diag_va("\n", s, argp);
-	va_end(argp);
+  va_list argp;
+  va_start(argp, s);
+  nerrors++;
+  diag_va("\n", s, argp);
+  va_end(argp);
 }
 
-void warning(char *s, ...) {
-	va_list argp;
-	va_start(argp, s);	
-	diag_va(" (warning)\n", s, argp);
-	va_end(argp);
+void warning(char* s, ...) {
+  va_list argp;
+  va_start(argp, s);
+  diag_va(" (warning)\n", s, argp);
+  va_end(argp);
 }
 
 void nofit(void) {
-	if (pass == PASS_3)
-		warning("too big");
+  if (pass == PASS_3) warning("too big");
 }
